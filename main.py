565925eb1cd07994f866b3a2fabc172435d71c04
MitchellMarczak <>
2021-04-15 21:13:15 +0000

    Final Version

--- main.py
+++ main.py
@@ -1,40 +1,48 @@
-print("------------------------------HISTORY TRIVIA GAME------------------------------")
+print("------------------------------U.S. HISTORY TRIVIA GAME------------------------------")
 print()
-print("                          Created by Mitchell Marczak\n")
+print("                            Created by Mitchell Marczak\n")
 print()
-
-print("The rules of this history trivia game are simple, answer using one of the options given, and have fun. All you need to do is put the letter for the answer you think is right, make sure not to use capitals. Good luck and have fun! Now let's get started!\n")
+print("The rules of this history trivia game are simple, answer using one of the options given, NO USING GOOGLE, and the final one is to have fun. All you need to do is put the letter (a, b, c, or d) for the answer you think is right, make sure not to use capitals. Good luck and have fun! Now let's get started!\n")
+ready_check = input("Are you ready to get started? Answer 'yes' or 'no'.\n")
+no = "no"
+yes = "yes"
+while ready_check != yes: 
+  print("\nThe rules of this history trivia game are simple, answer using one of the options given, NO USING GOOGLE, and the final one is to have fun. All you need to do is put the letter (a, b, c, or d) for the answer you think is right, make sure not to use capitals. Good luck and have fun! Now let's get started!\n")
+  ready_check = input("Are you ready to get started? (Answer 'yes' or 'no').\n")
+  no = "no"
+  yes = "yes"
+  print()
 g = open("Questions","w")
 
-g.write('Question 1: How many colonies did the U.S. consist of originally?\n')
-g.write('a. 8  \n')
-g.write('b. 12 \n')
-g.write('c. 10 \n')
-g.write('d. 13 \n')
+g.write('Question 1: What was the first state of the U.S.?\n')
+g.write('a. Georgia\n')
+g.write('b. New Jersey\n')
+g.write('c. Connecticut\n')
+g.write('d. Delaware\n')
 
-g.write('Question 2: \n')
-g.write('a.  \n')
-g.write('b.  \n')
-g.write('c.  \n')
-g.write('d.  \n')
+g.write('Question 2: How many states were there in the U.S. during the civil war?\n')
+g.write('a. 13\n')
+g.write('b. 50\n')
+g.write('c. 34\n')
+g.write('d. 28\n')
 
-g.write('Question 3: \n')
-g.write('a.  \n')
-g.write('b.  \n')
-g.write('c.  \n')
-g.write('d.  \n')
+g.write('Question 3: How many U.S. territories are there currently?\n')
+g.write('a. 16\n')
+g.write('b. 14\n')
+g.write('c. 8 \n')
+g.write('d. 19\n')
 
-g.write('Question 4: \n')
-g.write('a.  \n')
-g.write('b.  \n')
-g.write('c.  \n')
-g.write('d.  \n')
+g.write("Question 4: What was the name of President Abraham Lincoln's wife? \n")
+g.write('a. Mary Arthur  \n')
+g.write('b. Martha Wayles \n')
+g.write('c. Marian Dolley \n')
+g.write('d. Mary Todd \n')
 
-g.write('Question 5: \n')
-g.write('a.  \n')
-g.write('b.  \n')
-g.write('c.  \n')
-g.write('d.  \n')
+g.write('Question 5: Who created the national anthem (The Star-Spangled Banner) of the U.S.? \n')
+g.write('a. John McHenry\n')
+g.write('b. Francis Key\n')
+g.write('c. John Smith\n')
+g.write('d. Henry Key\n')
 g.close()
 
 g = open("Answers", "w")
@@ -42,93 +50,106 @@ g.write("d\n")
 g.write("c\n")
 g.write("a\n")
 g.write("d\n")
-g.write("a\n")
-
+g.write("b")
 g.close()
+
 read = open("Answers", "r")
-answer1 = read.read(1) 
-answer2 = read.read(2)
-answer3 = read.read(3)
-answer4 = read.read(4)
-answer5 = read.read(5)
+answers_set = (read.read(1), read.read(1), read.read(1), read.read(1), read.read(1), read.read(1), read.read(1), read.read(1), read.read(1))
 
 read = open("Questions", "r")
-
+print()
 #for i in range(0, 5):
   #print(read.readline())
-
+possible_choices = {"a", "b", "c", "d"}
 for i in range(0, 5):
   print(read.readline())
 
 correct_answers = 0
 incorrect_answers = 0
-question_1 = input()
+final_score = 0
+users_answer = input("Your answer: ")
+print()
+if users_answer in possible_choices:
+  if users_answer == answers_set[0]:
+    print(f"Your answer of '{answers_set[0]}' was correct!\n")
+    correct_answers += 1
+  else:
+    print(f"Your answer of '{users_answer}' was incorrect!\n")
+    incorrect_answers += 1
+else:
+  print("You won't be able to redo this question this attempt, so choose an actual answer next time.\n")
+  incorrect_answers += 1
+
+
+for i in range(0, 5):
+  print(read.readline())
+
+users_answer = input("Your answer: ")
 print()
 
-if question_1 == answer1:
-  print(f"Your answer of {answer1} was correct!\n")
-  correct_answers += 1
+if users_answer in possible_choices:
+  if users_answer == answers_set[2]:
+    print(f"Your answer of '{answers_set[2]}' was correct!\n")
+    correct_answers += 1
+  else:
+    print(f"Your answer of '{users_answer}' was incorrect!\n")
+    incorrect_answers += 1
 else:
-  print(f"Your answer of {question_1} was incorrect! The correct answer was {answer1}!\n")
+  print("You won't be able to redo this question this attempt, so choose an actual answer next time.\n")
   incorrect_answers += 1
 
 for i in range(0, 5):
   print(read.readline())
 
-question_2 = input()
+users_answer = input("Your answer: ")
 print()
-
-if question_2 == answer2:
-  print(f"Your answer of {answer2} was correct!\n")
-  correct_answers += 1
+if users_answer in possible_choices:
+  if users_answer == answers_set[4]:
+    print(f"Your answer of '{answers_set[4]}' was correct!\n")
+    correct_answers += 1
+  else:
+    print(f"Your answer of '{users_answer}' was incorrect!\n")
+    incorrect_answers += 1
 else:
-  print(f"Your answer of {question_2} was incorrect! The correct answer was {answer2}!\n")
+  print("You won't be able to redo this question this attempt, so choose an actual answer next time.\n")
   incorrect_answers += 1
 
 for i in range(0, 5):
   print(read.readline())
 
-question_3 = input()
+users_answer = input("Your answer: ")
 print()
-
-if question_3 == answer3:
-  print(f"Your answer of {answer3} was correct!\n")
-  correct_answers += 1
+if users_answer in possible_choices:
+  if users_answer == answers_set[6]:
+    print(f"Your answer of '{answers_set[6]}' was correct!\n")
+    correct_answers += 1
+  else:
+    print(f"Your answer of '{users_answer}' was incorrect!\n")
+    incorrect_answers += 1
 else:
-  print(f"Your answer of {question_3} was incorrect! The correct answer was {answer3}!\n")
+  print("You won't be able to redo this question this attempt, so choose an actual answer next time.\n")
   incorrect_answers += 1
 
 for i in range(0, 5):
   print(read.readline())
 
-question_4 = input()
+users_answer = input("Your answer: ")
 print()
-
-if question_4 == answer4:
-  print(f"Your answer of {answer4} was correct!\n")
-  correct_answers += 1
+if users_answer in possible_choices:
+  if users_answer == answers_set[8]:
+    print(f"Your answer of \"{answers_set[8]}\" was correct!\n")
+    correct_answers += 1
+  else:
+    print(f"Your answer of '{users_answer}' incorrect!\n")
+    incorrect_answers += 1
 else:
-  print(f"Your answer of {question_4} was incorrect! The correct answer was {answer4}!\n")
+  print("You won't be able to redo this question this attempt, so choose an actual answer next time.\n")
   incorrect_answers += 1
 
-for i in range(0, 5):
-  print(read.readline())
-
-question_5 = input()
-print()
-
-if question_5 == answer5:
-  print(f"Your answer of {answer5} was correct!\n")
-  correct_answers += 1
-else:
-  print(f"Your answer of {question_3} was incorrect! The correct answer was {answer5}!\n")
-  incorrect_answers += 1
-
-
-
-
-
-
-
-
-
+final_score = (correct_answers/(correct_answers + incorrect_answers) * 100)
+print("                                       All Done!                              ")
+print(f"                            Your final score was a {final_score}%!")  
+print('               You got %d questions correct and %d questions incorrect!' % (correct_answers, incorrect_answers))
+print("")
+print("                       I hope you enjoyed! Thanks for playing!\n ")
+print("                             Created by Mitchell Marczak")
